8e80e41f9a0632fface3c02c7a06edef2f538d12
Rose_Neopons <rose@neopons.com>
2026-04-24 10:22:34 +0900

    Align mobile Expo runtime setup with SDK 54

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,18 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+const appNodeModules = path.resolve(__dirname, "node_modules");
+const rootNodeModules = path.resolve(__dirname, "../../node_modules");
+const expoRouterNodeModules = path.join(appNodeModules, "expo-router/node_modules");
+
+config.resolver.disableHierarchicalLookup = true;
+config.resolver.nodeModulesPaths = [appNodeModules, expoRouterNodeModules, rootNodeModules];
+config.resolver.extraNodeModules = {
+  "@expo/metro-runtime": path.join(expoRouterNodeModules, "@expo/metro-runtime"),
+  react: path.join(appNodeModules, "react"),
+  "react-native": path.join(appNodeModules, "react-native")
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
